3ec6424948be82817113ec11d3b1606c923a6831
kbaan94 <60811769+kbaan94@users.noreply.github.com>
2022-04-26 02:28:41 -0400

    Update reportWebVitals.js
    Added link to where code was imported from.

--- React-app/Front-end/src/reportWebVitals.js
+++ React-app/Front-end/src/reportWebVitals.js
@@ -1,3 +1,5 @@
+//https://deepsource.io/gl/deepsourcelabs/demo-javascript/run/9370a6aa-6cba-4925-8ef5-1e46642c26f7/javascript/JS-0259/
+//Code used from the above link as a way to report web vitals
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
