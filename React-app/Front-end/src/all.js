4ed7e12da4c329330e32a670383d6c81456a6d87
kbaan94 <60811769+kbaan94@users.noreply.github.com>
2023-01-24 02:19:29 -0500

    Delete React-app/Front-end directory

--- React-app/Front-end/src/all.js
+++
@@ -1,8 +0,0 @@
-// HOOKS
-export {useInput,useAuth,useSpotify,useSpotifySetter} from 'hooks'
-
-// COMPONENTS
-export {Header,Footer,MediaControl,Playlist} from 'components'
-
-// VIEWS
-export {HomePage,AuthPage,SinglePage,SpotifyCallback} from 'views'
--- React-app/Front-end/src/hooks/index.js
+++
@@ -1,4 +0,0 @@
-export {default as useInput} from './useInput'
-export {default as useAuth} from './useAuth'
-export {default as useSpotify} from './useSpotify'
-export {default as useSpotifySetter} from './useSpotifySetter'
